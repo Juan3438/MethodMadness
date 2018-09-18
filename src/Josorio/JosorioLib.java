2c72ab8e995a784d34130032b93786e898a67c90
BT_1N3_13@bktech.local <BT_1N3_13@bktech.local>
2018-09-18 09:19:09 -0400

    Initial commit

---
+++ src/Josorio/JosorioLib.java
@@ -0,0 +1,12 @@
+package Josorio;
+
+public class JosorioLib {
+
+    public static void println(String str)
+    {
+        System.out.println(str);
+    }
+    public static String reverse(String var){
+        
+    }
+}
---
+++ src/Josorio/TextFile.java
@@ -0,0 +1,11 @@
+package Josorio;
+
+import static Josorio.JosorioLib.println;
+
+public class TextFile {
+
+    public static void main (String[] args)
+    {
+        println("I love a cup of Java in the morning");
+    }
+}
