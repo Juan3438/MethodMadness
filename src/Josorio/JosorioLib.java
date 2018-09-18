e06b1b3745f8310e00500c7bf8e70394d992d471
Juan3438 <Josorio3438@bths.edu>
2018-09-18 10:14:29 -0400

    First class update.

--- src/Josorio/JosorioLib.java
+++ src/Josorio/JosorioLib.java
@@ -2,11 +2,42 @@ package Josorio;
 
 public class JosorioLib {
 
-    public static void println(String str)
-    {
+    public static void println(String str) {
         System.out.println(str);
     }
-    public static String reverse(String var){
-        
+
+    public static String reverse(String var) {
+        String reversed = "";
+        for(int i = var.length()-1;i >= 0;i--)
+        {
+            String letter = var.substring(i,i+1);
+            reversed += letter;
+        }
+        return reversed;
+    }
+
+    public static boolean isFibonnaci(int num) {
+        int FibIdx = 1;
+        while(FibIdx < num)
+            FibIdx += FibIdx;
+        if(FibIdx == num)
+            return true;
+        else
+            return false;
+    }
+    public static int sumUpTo(int num){
+        int sum = 0;
+        for(int i = 0;i<num;i++){
+            sum += i;
+        }
+        return sum;
+    }
+    public static boolean isPalindrome(String str ){
+        String test = reverse(str);
+        return (test.equals(str));
+    }
+    public static String cutOut(String mainStr, String subStr){
+        String idx = mainStr.indexOf(subStr);
+        String result = mainStr.
     }
 }
--- src/Josorio/TextFile.java
+++ src/Josorio/TextFile.java
@@ -1,11 +1,19 @@
 package Josorio;
 
-import static Josorio.JosorioLib.println;
+import static Josorio.JosorioLib.*;
 
 public class TextFile {
 
-    public static void main (String[] args)
+    public static void main(String[] args)
     {
-        println("I love a cup of Java in the morning");
+        println("A series of tests!");
+        String test = reverse("mistakes");
+        println(test);
+        String test1 = isFibonnaci(8) + "";
+        println(test1);
+        String test2 = sumUpTo(20) + "";
+        println(test2);
+        String test3 = isPalindrome("poop") + "";
+        println(test3);
     }
 }
